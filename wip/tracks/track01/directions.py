cfaaa53487e63648bcbc16d8686254d528055c4d
nucularmoo <erika.jarvinen@helsinki.fi>
2018-10-19 23:22:14 +0300

    Create directions.py

---
+++ wip/tracks/track01/directions.py
@@ -0,0 +1,16 @@
+# Ramppi
+# Suoraan -> Vasen 90 astetta
+# Suoraan -> Vasen 90 astetta
+# Suoraan -> Oikea 90 astetta
+# Suoraan -> Oikea 90 astetta
+# Suoraan -> Oikea 45 astetta
+# Suoraan -> Oikea 45 astetta
+# Suoraan -> Ei vasen 0 astetta
+# Suoraan -> Keltainen 180 astetta
+# Suoraan -> Oikea 90 astetta
+# Suoraan -> Vasen 90 astetta
+# Suoraan -> Oikea 90 astetta
+# Suoraan -> Ei vasen 0 astetta
+# Suoraan -> Oikea 90 astetta
+# Suoraan -> Vasen 90 astetta
+# Ramppi
