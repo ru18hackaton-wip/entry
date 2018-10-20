c1f0873d03bb04d7332970884266dd73c33e4352
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-10-20 19:43:13 +0300

    Enable server?

--- wip/main.py
+++ wip/main.py
@@ -1,16 +1,20 @@
 from .robot import Robot
 from .runner import Runner
+from .www import Webserver
+from ev3dev2.control.webserver import WebControlledTank
 
 from .tracks import create_track01_tree
 
 def main():
     robot = Robot()
     runner = Runner(robot)
+    www = WebControlledTank()
 
     tree = create_track01_tree(robot)
     runner.set_tree(tree)
 
     runner.run()
+    www.main()
 
 if __name__ == "__main__":
     main()
