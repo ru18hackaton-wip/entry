c079c2911bc031c8194ebbd52841e9eb3bc2015a
Sami Saada <sami.saada@gmail.com>
2018-10-20 17:54:19 +0300

    match or not same color

--- wip/behaviors/follow_line.py
+++ wip/behaviors/follow_line.py
@@ -6,20 +6,22 @@ class FollowLine(Behaviour):
     def __init__(self, name="FollowLine"):
         super(FollowLine, self).__init__(name)
 
-    def setup(self, timeout, robot=None, color=None, speeds=None):
+    def setup(self, timeout, robot=None, color=None, speeds=None, color_match=None):
         if robot:
             self.robot = robot
         if color:
             self.target_color = color
         if speeds:
             self.target_speeds = speeds
+        if color_match != None:
+            self.color_match = color_match
         return True
 
     def initialise(self):
         pass
 
     def update(self):
-        if self.target_color == self.robot.get_color().color_name:
+        if (self.target_color == self.robot.get_color().color_name) == self.color_match:
             if self.status != Status.RUNNING:
                 left, right = self.target_speeds
                 self.robot.move(left,right)
