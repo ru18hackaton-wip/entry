8b7ca7f03b0836723f2c3841016e6cb9569848f8
Sami Saada <sami.saada@gmail.com>
2018-10-20 22:21:01 +0300

    vectors are friends

--- wip/ps4.py
+++ wip/ps4.py
@@ -2,8 +2,22 @@
 __author__ = "bythew3i"
 
 import evdev
-import ev3dev.auto as ev3
-import threading
+
+from ev3dev2.motor import MoveJoystick, LargeMotor, OUTPUT_A, OUTPUT_D
+
+def find_controller():
+    # ps4 controller set up
+    print("Finding ps4 controller...")
+    devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+    ps4dev = devices[0].fn
+
+    gamepad = evdev.InputDevice(ps4dev)
+    return gamepad
+
+def create_steering_tank():
+    jmove = MoveJoystick(OUTPUT_A, OUTPUT_D)
+    jmove.set_polarity(LargeMotor.POLARITY_INVERSED)
+    return jmove
 
 
 def scale(val, src, dst):
@@ -13,57 +27,13 @@ def scale_stick(value):
     return scale(value, (0,255),(-100,100))
 
 
-# ps4 controller set up
-print("Finding ps4 controller...")
-devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
-ps4dev = devices[0].fn
 
-gamepad = evdev.InputDevice(ps4dev)
-running = True
+gamepad = find_controller()
+bot = create_steering_tank()
 
-
-
-# wheel set up
-wheel_speed = 0
-
-class MotorThread(threading.Thread):
-    def __init__(self):
-        self.motor = ev3.LargeMotor(ev3.OUTPUT_A)
-        threading.Thread.__init__(self)
-
-    def run(self):
-        print("Wheels Ready...")
-        while running:
-            self.motor.run_direct(duty_cycle_sp=wheel_speed)
-
-        self.motor.stop()
-
-motor_thread = MotorThread()
-motor_thread.setDaemon(True)
-motor_thread.start()
-
-
-# steer set up
-steer_speed = 0
-
-class DirectionThread(threading.Thread):
-    def __init__(self):
-        self.motor = ev3.MediumMotor(ev3.OUTPUT_D)
-        threading.Thread.__init__(self)
-
-    def run(self):
-        print("Steer Ready...")
-        while running:
-            self.motor.run_direct(duty_cycle_sp=steer_speed)
-
-steer_thread = DirectionThread()
-steer_thread.setDaemon(True)
-steer_thread.start()
-
-
-# event listner
 for event in gamepad.read_loop():   #this loops infinitely
-    print(event.type)
+    print(event)
+    print(repr(event))
     if event.type == 3:             #A stick is moved
         if event.code == 5:         #Y axis on right stick
             wheel_speed = scale_stick(event.value)
@@ -74,4 +44,3 @@ for event in gamepad.read_loop():   #this loops infinitely
         print("X button is pressed. Stopping.")
         running = False
         break
-
